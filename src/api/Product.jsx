5685daa9aadb99f7b992f2fc4d127a00df1085da
sayeda-chattopadhyay <83353551+sayeda-chattopadhyay@users.noreply.github.com>
2023-05-18 16:21:30 +0200

    creates cart, 404 and product page

---
+++ src/api/Product.jsx
@@ -0,0 +1,12 @@
+// import React from 'react'
+
+
+// const FetchSingleProduct = () => {
+
+//     let params = useParams();
+//   return (
+//     <div>Single product ID : {params.id}</div>
+//   )
+// }
+
+// export default FetchSingleProduct
--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const NotFound = () => {
-  return (
-    <div>NotFound</div>
-  )
-}
+  return <div> Page Not Found</div>;
+};
 
-export default NotFound
+export default NotFound;
