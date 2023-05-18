033031c432c44699be5862a6168f522645a8e73b
sayeda-chattopadhyay <83353551+sayeda-chattopadhyay@users.noreply.github.com>
2023-05-18 11:49:56 +0200

    creates navbar

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
-  )
-}
+    <div className="navbar">
+      <nav>
+        <h1>la-Boutique</h1>
+        <NavLink to="/">Home</NavLink>
+        {/* <NavLink to="about">About</NavLink>
+        <NavLink to="help">Help</NavLink>
+        <NavLink to="careers">Careers</NavLink> */}
+      </nav>
+    </div>
+  );
+};
 
-export default NavBar
+export default NavBar;
