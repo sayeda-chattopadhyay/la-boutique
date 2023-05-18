d8de929261655f18412ded61d0e6552c93aef844
sayeda-chattopadhyay <83353551+sayeda-chattopadhyay@users.noreply.github.com>
2023-05-18 16:20:29 +0200

    adds route for home, contact,cart & 404 pages.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,35 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 
-// import Home from "./pages/Home";
+
 
 // layout
 
 import BaseLayOut from "./layouts/BaseLayout";
 
+// pages
+
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+import Cart from "./pages/Cart";
+import NotFound from "./pages/NotFound";
+
+
+// Api
+
+// import FetchSingleProduct from "./components/FetchSingleProduct";
+
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<BaseLayOut />} />
+        <Route path="/" element={<BaseLayOut />}>
+          <Route index element={<Home />}/>
+
+          <Route path="/Contact" element={<Contact />} />
+          <Route path="/Cart" element={<Cart />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
       </Routes>
     </div>
   );
