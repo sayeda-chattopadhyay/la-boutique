809fc8c5e1908c935fe915b160a5b79bae757bc9
sayeda-chattopadhyay <83353551+sayeda-chattopadhyay@users.noreply.github.com>
2023-05-18 11:50:27 +0200

    adds BaseLayout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
 
-import Home from "./pages/Home";
+// import Home from "./pages/Home";
+
+// layout
+
+import BaseLayOut from "./layouts/BaseLayout";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Routes>
+        <Route path="/" element={<BaseLayOut />} />
+      </Routes>
     </div>
   );
 }
