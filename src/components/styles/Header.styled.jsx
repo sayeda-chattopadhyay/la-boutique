9a7bec16461e94198a2b4861c141543a998bf703
sayeda-chattopadhyay <83353551+sayeda-chattopadhyay@users.noreply.github.com>
2023-05-20 22:39:02 +0200

    adds header and nav styles

---
+++ src/components/styles/Header.styled.jsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const StyledHeader = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px;
+  border: 1px solid lightgray;
+`;
+
+export const Logo = styled.img`
+  max-width: 100%;
+  width: 100px;
+  height: auto;
+`;
