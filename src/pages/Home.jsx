d412aa2824bb2be9e0a9a920e235a2cd725d5756
sayeda-chattopadhyay <83353551+sayeda-chattopadhyay@users.noreply.github.com>
2023-05-23 15:20:28 +0200

    removes search component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,4 @@
 import Banner from "../components/banner/Banner";
-import Search from "../components/search/Search";
 
 import Products from "../api/Products";
 
@@ -7,7 +6,6 @@ const Home = () => {
   return (
     <div className="home">
       <Banner />
-      <Search />
       <Products />
     </div>
   );
