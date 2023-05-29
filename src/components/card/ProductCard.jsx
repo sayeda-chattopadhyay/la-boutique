a2186690f9242acb4935f9283f4256af2fae5703
sayeda-chattopadhyay <83353551+sayeda-chattopadhyay@users.noreply.github.com>
2023-05-29 23:55:02 +0200

    adds indivitual product card

--- src/components/card/ProductCard.jsx
+++ src/components/card/ProductCard.jsx
@@ -1,6 +1,9 @@
 import { StyledCard } from "../styles/Card.styled";
-import { StyledButton } from "../styles/Button.styled";
+// import { StyledButton } from "../styles/Button.styled";
 import ProductReviews from "../../components/review/ProductReviews";
+import { useContext } from "react";
+import { CartContext } from "../../CartContext";
+import { Button, Form, Row, Col } from "react-bootstrap";
 
 export default function ProductCard({
   product: {
@@ -13,6 +16,11 @@ export default function ProductCard({
     reviews,
   },
 }) {
+  const cart = useContext(CartContext);
+  const productQuantity = cart.getProductQuantity(id);
+  console.log(productQuantity);
+  console.log(cart.items);
+
   function calculateDiscountPercentage(price, discountedPrice) {
     if (price && discountedPrice) {
       const discount = price - discountedPrice;
@@ -46,9 +54,48 @@ export default function ProductCard({
           <p>Discounted Price: {discountedPrice}</p>
         </div>
         <ProductReviews reviews={reviews} />
-        <div>
-          <StyledButton>Add to cart</StyledButton>
-        </div>
+        {productQuantity > 0 ? (
+          <>
+            <Form as={Row}>
+              <Form.Label column="true" sm="6">
+                 {productQuantity} {title} added to cart
+              </Form.Label>
+              <Col sm="6">
+                <Button
+                  sm="6"
+                  onClick={() => cart.addOneToCart(id)}
+                  className="mx-2"
+                >
+                  +
+                </Button>
+                {productQuantity}
+                <Button
+                  sm="6"
+                  onClick={() => cart.removeOneFromCart(id)}
+                  className="mx-2"
+                >
+                  -
+                </Button>
+              </Col>
+            </Form>
+            <Button
+              variant="danger"
+              onClick={() => cart.deleteFromCart(id)}
+              className="my-2"
+            >
+              Remove From Cart
+            </Button>
+          </>
+        ) : (
+          <Button variant="primary" onClick={() => cart.addOneToCart(id)}>
+            Add To Cart
+          </Button>
+        )}
+        {/* <div>
+          <StyledButton onClick={() => cart.addOneToCart(id)}>
+            Add to cart
+          </StyledButton>
+        </div> */}
       </StyledCard>
     </>
   );
