c71967d02b49cb7188ad70973916a7aa7c690497
sayeda-chattopadhyay <83353551+sayeda-chattopadhyay@users.noreply.github.com>
2023-06-06 19:06:13 +0200

    removes modal from Navbar

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -4,15 +4,10 @@ import { AiOutlineShoppingCart } from "react-icons/ai";
 import { useContext } from "react";
 import { CartContext } from "../../CartContext";
 import { Link } from "react-router-dom";
-//import CartProduct from "../cart/CartProduct";
 
 function NavbarComponent() {
   const cart = useContext(CartContext);
   const productsCount = cart.calculateProductsCount();
-
-  // const [show, setShow] = useState(false);
-  // const handleClose = () => setShow(false);
-  // const handleShow = () => setShow(true);
 
   return (
     <>
@@ -21,10 +16,7 @@ function NavbarComponent() {
           la-boutique
         </Navbar.Brand>
         <Navbar.Toggle className="bg-white" />
-        <Navbar.Collapse
-          // id="basic-navbar-nav"
-          className="justify-content-between text-white"
-        >
+        <Navbar.Collapse className="justify-content-between text-white">
           <Nav className="ms-auto">
             <Link to="/" className="text-white">
               Home
@@ -35,45 +27,10 @@ function NavbarComponent() {
             <Link to="/Cart" className="text-white">
               <AiOutlineShoppingCart className="cart-icon" />
               {productsCount}
-              {/* {cart.calculateProductsCount()} */}
             </Link>
-            {/* <Button variant="info" onClick={handleShow}>
-              {productsCount}
-              <AiOutlineShoppingCart />
-            </Button> */}
           </Nav>
         </Navbar.Collapse>
       </Navbar>
-
-      {/* <Modal show={show} onHide={handleClose}>
-        <div>
-          <Modal.Header closeButton className="bg-dark">
-            <Modal.Title>Shopping Cart</Modal.Title>
-          </Modal.Header>
-          <Modal.Body className="bg-dark">
-            {productsCount > 0 ? (
-              <>
-                <p>Items in your cart:</p>
-                {cart.items.map((currentProduct, idx) => (
-                  <CartProduct
-                    key={idx}
-                    id={currentProduct.id}
-                    title={currentProduct.title} // title doesn't come
-                    quantity={currentProduct.quantity}
-                    price={currentProduct.price}
-                  ></CartProduct>
-                ))}
-
-                <h1>Total: {cart.getTotalCost().toFixed(2)}</h1>
-
-                <Button variant="success">Purchase items!</Button>
-              </>
-            ) : (
-              <h1>There are no items in your cart!</h1>
-            )}
-          </Modal.Body>
-        </div>
-      </Modal> */}
     </>
   );
 }
