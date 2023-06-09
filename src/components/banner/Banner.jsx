2da3113f9d3d39de771b17bba00380a325235452
sayeda-chattopadhyay <83353551+sayeda-chattopadhyay@users.noreply.github.com>
2023-06-09 10:40:54 +0200

    removes explore button

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -1,20 +1,22 @@
-
 import { StyledBanner } from "../styles/Banner.styled";
-import {StyledButton} from "../styles/Button.styled";
+//import { StyledButton } from "../styles/Button.styled";
+//import { Link } from "react-router-dom";
 
 const Banner = () => {
   return (
     <StyledBanner>
       <div>
-        <h4>Discover a World of Amazing Products</h4>
-        <h2>Welcome to Our Online Store!</h2>
+        <h5>Discover a World of Amazing Products</h5>
+        <h1>Welcome to Our Online Store!</h1>
         <p>
           la-boutique offers a wide range of high-quality products, carefully
           curated to cater to your needs and preferences. Whether you're looking
           for the latest fashion trends, electronics, home essentials, or unique
           gifts, we have something for everyone
         </p>
-        <StyledButton>Explore</StyledButton>
+        {/* <Link to="/src/components/card/ProductCard.jsx">
+          <StyledButton>Explore</StyledButton>
+        </Link> */}
       </div>
       <div className="banner-image">
         <img src="./images/banner.jpg" alt="Banner" />
